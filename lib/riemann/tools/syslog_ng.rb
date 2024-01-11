aace3643a2dc08298aada37d0c2c3f057af9652d
Romain Tartière <romain@blogreen.org>
2024-01-10 17:59:47 -1000

    Split complex functions in smaller parts

--- lib/riemann/tools/syslog_ng.rb
+++ lib/riemann/tools/syslog_ng.rb
@@ -43,11 +43,11 @@ module Riemann
         while (line = @socket.gets.chomp) != '.'
           source_name, source_id, source_instance, state, type, metric = line.split(';')
 
-          next if opts[:source_name] && !opts[:source_name].include?(source_name)
-          next if opts[:source_id] && !opts[:source_id].include?(source_id)
-          next if opts[:source_instance] && !opts[:source_instance].include?(source_instance)
-          next if opts[:state] && !opts[:state].include?(state)
-          next if opts[:type] && !opts[:type].include?(type)
+          next if rejected_source_name?(source_name)
+          next if rejected_source_id?(source_id)
+          next if rejected_source_instance?(source_instance)
+          next if rejected_state?(state)
+          next if rejected_type?(type)
 
           res << {
             source_name: source_name,
@@ -62,17 +62,45 @@ module Riemann
         res
       end
 
+      def rejected_source_name?(source_name)
+        opts[:source_name] && !opts[:source_name].include?(source_name)
+      end
+
+      def rejected_source_id?(source_id)
+        opts[:source_id] && !opts[:source_id].include?(source_id)
+      end
+
+      def rejected_source_instance?(source_instance)
+        opts[:source_instance] && !opts[:source_instance].include?(source_instance)
+      end
+
+      def rejected_state?(state)
+        opts[:state] && !opts[:state].include?(state)
+      end
+
+      def rejected_type?(type)
+        opts[:type] && !opts[:type].include?(type)
+      end
+
       def statistic_state(type, metric)
         if type == 'dropped'
-          metric == 0.0 ? 'ok' : 'critical'
+          dropped_statistic_state(metric)
         elsif type == 'queued'
-          if metric >= opts[:queued_critical]
-            'critical'
-          elsif metric >= opts[:queued_warning]
-            'warning'
-          else
-            'ok'
-          end
+          queued_statistic_state(metric)
+        end
+      end
+
+      def dropped_statistic_state(metric)
+        metric == 0.0 ? 'ok' : 'critical'
+      end
+
+      def queued_statistic_state(metric)
+        if metric >= opts[:queued_critical]
+          'critical'
+        elsif metric >= opts[:queued_warning]
+          'warning'
+        else
+          'ok'
         end
       end
     end
